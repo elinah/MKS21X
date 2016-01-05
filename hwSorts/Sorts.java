e4623bda894c07f55c4aea88ff37b1dc0e32f9e6
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-01-05 12:23:08 -0500

    Added insertion sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -1,5 +1,5 @@
 public class Sorts{
-    public static void selection(int[] data){;
+    public static void selectionSort(int[] data){;
 	int atCur;
 	for(int cur=0;cur<data.length;cur++){
 	    int min = cur;
@@ -11,5 +11,14 @@ public class Sorts{
 	    data[cur] = data[min];
 	    data[min] = atCur;
 	}
+    }
+
+    public static void insertionSort(int[] data){
+	for(int cur=0;ctr<data.length;ctr++){
+	    for(int i=cur+1;i<data.length;i++){
+		if(data[cur] > data[i]){
+		    atCur = data[cur];
+		    data[cur] = data[i];
+		    data[i] = data[cur];
     }
 }
